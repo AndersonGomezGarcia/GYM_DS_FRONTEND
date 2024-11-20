23003ae45bcb9547cacf1fc4a616a9919bc05102
Joseph <josephherrera1407@gmail.com>
2024-11-20 01:56:11 -0500

    Se modifica el login para acomodar el boton de link.

--- gym_ds/src/components/login.js
+++ gym_ds/src/components/login.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './login.css';
 import referenceImage from './gym.jpg'; 
 import { Link } from 'react-router-dom';
+import Signup from './sign-up';
 
 function Login() {
   return (
     <div className="login-container">
       <div className="form-container">
         <h2>Welcome to the gym:</h2>
-        <h3>Please Register or Login</h3>
         <form>
           <div className="form-group">
             <label htmlFor="email">Email:</label> <br />
@@ -20,13 +20,14 @@ function Login() {
           </div>
           <button type="submit">Login</button>
         </form>
+        <div className="link-sign_up">
+        ¿No tienes una cuenta? <Link to="/signup">Regístrate aquí</Link> {Signup}
+      </div>
       </div>
       <div className="image-container">
         <img src={referenceImage} alt="Reference" />
       </div>
-      <div className="link-sign_up">
-        ¿No tienes una cuenta? <Link to="./sign-up.js">Regístrate aquí</Link> {/* Cambia <a> por <Link> */}
-      </div>
+
     </div>
   );
 }
